687367778a3f48c4959e12b4ac9c7f12236cf173
scytrowski <szym.cytrowski@gmail.com>
2025-06-29 00:41:38 +0200

    add docs

--- src/main/scala/io/github/scytrowski/mat/Materialize.scala
+++ src/main/scala/io/github/scytrowski/mat/Materialize.scala
@@ -3,19 +3,43 @@ package io.github.scytrowski.mat
 import scala.compiletime.{error, summonFrom}
 import scala.deriving.Mirror
 
+/** Attempts to materialize type `A`
+  *
+  * In case of failure a compilation error is raised
+  */
 transparent inline def materialize[A]: Any =
   inline materializeOpt[A] match
     case Some(a) => a
     case None    => error("Type cannot be materialized")
 
+/** Safely attempts to materialize type `A`
+  */
 transparent inline def materializeOpt[A]: Any =
   summonFrom {
     case mat: Materialize[A] => Some(mat())
     case _                   => None
   }
 
+/** Evidence that a type `A` can be materialized into a value at compile time.
+  *
+  * A type `A` is considered materializable if it satisfies one of the following
+  * conditions:
+  *
+  *   - It is a constant type (i.e., there exists a `ValueOf[A]`)
+  *   - It is a tuple (`Tuple`) composed entirely of materializable elements
+  *   - It is a product type (e.g., a case class) whose fields are all
+  *     materializable
+  *   - It is a sum type (e.g., a sealed trait or enum) with exactly one
+  *     materializable variant
+  *
+  * The `apply()` method returns a value of type `Out`, which is guaranteed to
+  * be a subtype of `A`.
+  */
 sealed trait Materialize[A]:
+  /** The resulting type of the materialized value. */
   type Out <: A
+
+  /** Materializes a value of type `Out`, satisfying the structure of `A`. */
   def apply(): Out
 
 object Materialize extends LowPriorityMaterialize:
